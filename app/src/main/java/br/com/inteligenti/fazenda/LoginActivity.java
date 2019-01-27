08da79edf227c381d6c94bbaca0105f98e841fe1
Joao Fernando Rocha <fernandorochaworld@gmail.com>
2019-01-27 20:55:53 -0200

    initial commit

---
+++ app/src/main/java/br/com/inteligenti/fazenda/LoginActivity.java
@@ -0,0 +1,24 @@
+package br.com.inteligenti.fazenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_login);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+//Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+//set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_login);
+    }
+}
