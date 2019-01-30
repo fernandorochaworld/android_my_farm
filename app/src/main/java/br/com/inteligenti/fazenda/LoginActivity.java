2d931ba08d98e3761bb58bcf7e7fc1a535c5f967
Joao Fernando Rocha <fernandorochaworld@gmail.com>
2019-01-30 03:21:08 -0200

    Start Firebase

--- app/src/main/java/br/com/inteligenti/fazenda/LoginActivity.java
+++ app/src/main/java/br/com/inteligenti/fazenda/LoginActivity.java
@@ -7,7 +7,17 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +32,48 @@ public class LoginActivity extends AppCompatActivity {
 
 //set content view AFTER ABOVE sequence (to avoid crash)
         this.setContentView(R.layout.activity_login);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 
     public void signup(View view)
     {
         startActivity(new Intent(this, SignupActivity.class));
+    }
+
+    public void signin(View view)
+    {
+        writeNewUser("U001", "Fernando Rocha", "fernandorochaworld@gmail.com");
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+
+        mDatabase.child("users").child(userId).setValue(user.toMap());
+    }
+
+    @IgnoreExtraProperties
+    public class User {
+
+        public String username;
+        public String email;
+
+        public User() {
+            // Default constructor required for calls to DataSnapshot.getValue(User.class)
+        }
+
+        public User(String username, String email) {
+            this.username = username;
+            this.email = email;
+        }
+
+        @Exclude
+        public Map<String, Object> toMap() {
+            HashMap<String, Object> result = new HashMap<>();
+            result.put("username", username);
+            result.put("email", email);
+            return result;
+        }
+
     }
 }
